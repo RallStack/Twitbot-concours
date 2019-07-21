df8ff87a221ef3706770785dcd2e190309d94ac3
Maxime <admin@admin.com>
2019-07-21 16:50:48 +0200

    Add follow system

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ api = twitter.Api(consumer_key=config.CONSUMER_KEY,
 def log(text=""):
     date = datetime.datetime.now()
 
-    file = open(config.LOG_PATH + date.year + '_' + date.month + '_' + date.day, "a+")
+    file = open(config.LOG_PATH+date.year+'_'+date.month+'_'+date.day, "a+")
     file.write(text)
     file.close()
+
+def Follow(post):
+    text = post.text
+
+    if "follow" in text:
+        try:
+            api.CreateFriendship(user_id=post.user.id)
+        except:
+            log("error: cannot follow user : "+post.user.screen_name)
 
 list = api.GetSearch(term="RT et follow", count=25, lang="fr")
