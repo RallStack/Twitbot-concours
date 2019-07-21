15f6ad861572fa39f72ab09505f5bc09f396891d
Maxime <admin@admin.com>
2019-07-21 14:26:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import  twitter
+import config
+
+api = twitter.Api(consumer_key=config.CONSUMER_KEY,
+                  consumer_secret=config.CONSUMER_SECRET,
+                  access_token_key=config.ACCESS_TOKEN,
+                  access_token_secret=config.ACCESS_TOKEN_SECRET)
+
+api.GetSearch(term="RT et follow")
