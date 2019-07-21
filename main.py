f5a1353948ac7ec5774dd9709f559e3e4db7cf2d
Maxime <admin@admin.com>
2019-07-21 16:44:11 +0200

    Add logging system

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import  twitter
+import twitter
 import config
+import datetime
 
 api = twitter.Api(consumer_key=config.CONSUMER_KEY,
                   consumer_secret=config.CONSUMER_SECRET,
                   access_token_key=config.ACCESS_TOKEN,
                   access_token_secret=config.ACCESS_TOKEN_SECRET)
 
-api.GetSearch(term="RT et follow")
+def log(text=""):
+    date = datetime.datetime.now()
+
+    file = open(config.LOG_PATH + date.year + '_' + date.month + '_' + date.day, "a+")
+    file.write(text)
+    file.close()
+
+list = api.GetSearch(term="RT et follow", count=25, lang="fr")
